8cf374c665b7e4bed6dbce6976217226868c6460
ToastCoder <vicky.pcbasic@gmail.com>
2020-11-05 15:26:25 +0530

    Fewer changes

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-# REQUIRED MODULES
+# STOCK PREDICTION USING ML
+
+# DEVELOPED BY: 
+# MOULISHANKAR M.R
+# VIGNESHWAR RAVICHANDAR
+
+# IMPORTING REQUIRED MODULES
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime as dt
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
 
+# IMPORTING DATA
