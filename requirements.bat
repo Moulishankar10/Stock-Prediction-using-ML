6a995b99ccd5ef0274821174c755b25410da52a4
Moulishankar10 <moulishankarmr@gmail.com>
2020-11-11 14:51:57 +0530

    Created requirements.bat

---
+++ requirements.bat
@@ -0,0 +1,13 @@
+echo " "
+echo "requirements.bat prompts you to install the modules required for this program!"
+echo " "
+
+pip3 install numpy
+pip3 install pandas
+pip3 install sklearn
+pip3 install datetime
+pip3 install matplotlib
+
+echo " "
+echo "Now, you are all set to execute this program!"
+echo " "
