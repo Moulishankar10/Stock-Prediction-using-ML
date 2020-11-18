ffffdfe9e15f84e5e96479ff5acae582ef990fea
Moulishankar10 <moulishankarmr@gmail.com>
2020-11-18 17:55:44 +0530

    Updated the code

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def mlmodel():
     # DISPLAYING RESULTS
     print(f"\nThe Predicted Quantity of {input_product} to be sold on {input_month} -->> {round(float(res))}")
     
-
+    """
     # TO VISUALISE THE ACCURACY
     
     ## ACCURACY SCORE PREDICTION
@@ -67,7 +67,7 @@ def mlmodel():
     plt.plot(x_grid, regressor.predict(x_grid), color = 'blue')
     plt.scatter(x_pred, res, color='green')
     plt.show()
-    
+    """
 
 print(
     '''
