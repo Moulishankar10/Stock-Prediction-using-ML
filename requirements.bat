f38fe990d14b2a99b9165effaa2fb85d2f33cfc9
Moulishankar10 <moulishankarmr@gmail.com>
2020-12-14 17:01:03 +0530

    Modified the batch file

--- requirements.bat
+++ requirements.bat
@@ -1,9 +1,5 @@
-" Checking the Required Dependencies ! "
-
 pip install numpy
 pip install pandas
 pip install sklearn
 pip install datetime
 pip install matplotlib
-
-"Now, you are all set to execute this program!"
